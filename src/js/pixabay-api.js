e4d241ff8fc541320236223e21236ac852edc903
VladyslavBihun <bihunvv@gmail.com>
2024-04-14 17:21:57 +0300

    first part has done

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,20 +1,21 @@
 import axios from "axios";
 
 axios.defaults.baseURL = "https://pixabay.com/api/";
-const key = 'key=43311395-53aae2d6cf7a92acb61eb4f46';
+const key = '43311395-53aae2d6cf7a92acb61eb4f46';
     
 export async function searchImage(userValue) {
 
-    const response = await axios.get(`?${key}`, {
-        params: {
-            q: userValue,
-            image_type: "photo",
-            orientation: "horizontal",
-            safesearch: true,
-            }
-    });
-    
-    return response.data;
+        const response = await axios.get(``, {
+            params: {
+                key,
+                q: userValue,
+                image_type: "photo",
+                orientation: "horizontal",
+                safesearch: true,
+                }
+        });
+
+        return response.data;
 
 };
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
-import axios from "axios";
-
 import { createMarkup } from './js/render-functions';
 import { searchImage } from './js/pixabay-api';
 
@@ -13,30 +11,36 @@ const loader = document.querySelector(".loader")
 
 form.addEventListener("submit", handleSubmit);
 
-function handleSubmit(event) {
+async function handleSubmit(event) {
     event.preventDefault();
     loader.style.display = 'inline-block';
 
     const inputValue = input.value;
 
-    searchImage(inputValue)
-        .then(data => createMarkup(data.hits, list))
-        .catch(() => {
-            list.innerHTML = '';
-            iziToast.error({
-        message: `Sorry, there are no images matching your search query. Please try again!`,
-        theme: 'dark',
-        messageColor: '#fff',
-        messageSize: '16px',
-        messageLineHeight: '1.5',
-        backgroundColor: '#EF4040',
-        position: 'topRight',
-        progressBarColor: '#B51B1B',
-            });
-        })
-        .finally(() => {
-            loader.style.display = 'none';
-            form.reset();
-        });
-};
+    try {
+        const imageData = await searchImage(inputValue);
 
+        if (imageData.hits.length === 0) {
+            throw new Error;
+        }
+
+        createMarkup(imageData.hits, list);
+
+    } catch (error) {
+        list.innerHTML = '';
+        iziToast.error({
+            message: `Sorry, there are no images matching your search query. Please try again!`,
+            theme: 'dark',
+            messageColor: '#fff',
+            messageSize: '16px',
+            messageLineHeight: '1.5',
+            backgroundColor: '#EF4040',
+            position: 'topRight',
+            progressBarColor: '#B51B1B',
+        });
+        
+    } finally {
+        loader.style.display = 'none';
+        form.reset();
+    }
+};
